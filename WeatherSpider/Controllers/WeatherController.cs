747426bc868db533a7dd0e9bf88ddbcc6568b300
Jaseela93 <jaseelajamal93@gmail.com>
2023-02-24 23:34:04 +0400

    feb 24

--- WeatherSpider/Controllers/WeatherController.cs
+++ WeatherSpider/Controllers/WeatherController.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+// to create web request
+using System.Net;
+using System.Net.Http;
 namespace WeatherSpider.Controllers
 {
     public class WeatherController : Controller
     {
         public IActionResult Index()
         {
+            var url = "https://meteostat.p.rapidapi.com/stations/monthly?station=72502&start=2021-01-01&end=2021-12-31";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method= "GET";
+
+            request.Headers["X-RapidAPI-Key"] = "a975adad98msh92c4681ebfe3889p12b590jsn197f8d456368";
+            request.Headers["X-RapidAPI-Host"] = "meteostat.p.rapidapi.com";
+            request.UseDefaultCredentials = true;
+            request.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
+
+            using var webResponse = request.GetResponse();
+
+            using var webStream = webResponse.GetResponseStream();
+
+            using var reader = new StreamReader(webStream);
+            var data = reader.ReadToEnd();
+
             return View();
         }
     }
